6ff30d216b2610aaf4e6adb7a55928d500e5cfc7
Jwata <watanabe0621@gmail.com>
2020-05-24 23:18:50 +0900

    implement ECDSA keygen

--- src/lib/ecdsa.spec.ts
+++ src/lib/ecdsa.spec.ts
@@ -94,65 +94,44 @@ describe('MPCEC', function() {
       });
     });
   });
-  it('generates secret key destributedly', async function() {
-    const session = LocalStorageSession.init('test_ec_keygen');
-    const p1 = new Party(1, session);
-    const p2 = new Party(2, session);
-    const p3 = new Party(3, session);
-    const dealer = new Party(999, session);
-    const conf = { n: 3, k: 2 };
+  describe('keyGen', function() {
+    it('generates private key shares', async function() {
+      const session = LocalStorageSession.init('test_ec_keygen');
+      const p1 = new Party(1, session);
+      const p2 = new Party(2, session);
+      const p3 = new Party(3, session);
+      const dealer = new Party(999, session);
+      const conf = { n: 3, k: 2 };
 
-    // All participants connect to the network
-    p1.connect();
-    p2.connect();
-    p3.connect();
-    dealer.connect();
+      // All participants connect to the network
+      p1.connect();
+      p2.connect();
+      p3.connect();
+      dealer.connect();
 
-    // Party
-    for (let p of [p1, p2, p3]) {
-      background(async () => {
-        const mpc = new MPC(p, conf);
+      // Party
+      for (let p of [p1, p2, p3]) {
+        background(async () => {
+          const mpc = new ecdsa.MPCECDsa(p, conf, ec);
 
-        // generate priv key shares
-        const priv = new Share('priv', p.id);
-        await mpc.rand(priv);
+          await mpc.keyGen()
 
-        // calcluate Pub = priv * G locally
-        const privHex = priv.value.toString(16);
-        const keyPair = ec.keyFromPrivate(privHex, 'hex');
+          // send private key share for assertion
+          await mpc.p.sendShare(mpc.privateKey, p1.id);
 
-        const pub = keyPair.getPublic();
-        const xHex = `0x${pub.getX().toJSON()}`;
-        const pubX = new Share('pubX', p.id, xHex);
-        const yHex = `0x${pub.getY().toJSON()}`;
-        const pubY = new Share('pubY', p.id, yHex);
+          // Party1 reconstructs keyPair on behalf of the parties.
+          if (p != p1) return;
 
-        await mpc.p.sendShare(priv, dealer.id);
-        await mpc.p.sendShare(pubX, dealer.id);
-        await mpc.p.sendShare(pubY, dealer.id);
-      });
-    }
-
-    // Dealer
-    await background(async () => {
-      const priv = new Secret('priv');
-      const pubX = new Secret('pubX');
-      const pubY = new Secret('pubY');
-
-      // recieve result shares from parties
-      const points: Array<[number, ecdsa.ECPoint]> = [];
-      for (let pId of [1, 2, 3]) {
-        await dealer.receiveShare(priv.getShare(pId));
-        await dealer.receiveShare(pubX.getShare(pId));
-        await dealer.receiveShare(pubY.getShare(pId));
-        const P = ec.keyFromPublic({
-          x: pubX.getShare(pId).value.toString(16),
-          y: pubY.getShare(pId).value.toString(16)
-        }).getPublic();
-        points.push([pId, P]);
+          const priv = new Secret('privateKey');
+          for (let pId of [1, 2, 3]) {
+            await p.receiveShare(priv.getShare(pId));
+          }
+          expectToBeReconstructable(priv);
+          const pub = mpc.curve.keyFromPrivate(
+            priv.value.toString(16)).getPublic();
+          expect(mpc.publicKey.eq(pub)).toBeTruthy();
+        });
       }
-      expectToBeReconstructable(priv);
-      expectToBeReconstructablePubkey(priv, points);
     });
   });
 });
